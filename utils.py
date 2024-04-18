f8fdaa6b4fd86b17455677569e66e2915b49c519
naush01 <r12922a05@ntu.edu.tw>
2024-04-18 12:51:22 +0800

    replace Image.ANTIALIAS with Image.LANCZOS

--- utils.py
+++ utils.py
@@ -14,9 +14,9 @@ load = transforms.ToTensor()
 def load_image(filename, size=None, scale=None):
     img = Image.open(filename)
     if size is not None:
-        img = img.resize((size, size), Image.ANTIALIAS)
+        img = img.resize((size, size), Image.LANCZOS)
     elif scale is not None:
-        img = img.resize((int(img.size[0] / scale), int(img.size[1] / scale)), Image.ANTIALIAS)
+        img = img.resize((int(img.size[0] / scale), int(img.size[1] / scale)), Image.LANCZOS)
     return img
 
 
