33f14f28941250f7d648082ac6819f00627799eb
naush01 <r12922a05@ntu.edu.tw>
2024-04-18 13:19:59 +0800

    replace 16 with module_size in get_3DGauss()

--- utils.py
+++ utils.py
@@ -149,8 +149,8 @@ def get_3DGauss(module_size, s=0, e=15, sigma=1.5, mu=7.5):
     x, y = np.mgrid[s:e:module_size * 1j, s:e:module_size * 1j]
     z = (1 / (2 * math.pi * sigma ** 2)) * np.exp(-((x - mu) ** 2 + (y - mu) ** 2) / (2 * sigma ** 2))
     z = torch.from_numpy(MaxMinNormalization(z.astype(np.float32)))
-    for j in range(16):
-        for i in range(16):
+    for j in range(module_size):
+        for i in range(module_size):
             if z[i, j] < 0.1:
                 z[i, j] = 0
     return z
